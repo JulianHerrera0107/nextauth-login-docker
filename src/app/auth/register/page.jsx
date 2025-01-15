27e44a7923805793987157ba7e507400496d1886
Julian-Herrera <juliandavih@gmail.com>
2025-01-15 16:05:30 -0500

    Creando el formulario de registro de la App utilizando el hook de react-hook-form para capturar los datos con handlesubmit y register acompañado de mensajes de error. Preparando el envío de datos en la Api Auth

---
+++ src/app/auth/register/page.jsx
@@ -0,0 +1,102 @@
+"use client"
+import { useForm } from "react-hook-form"
+
+function RegisterPage() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm();
+
+    const onSubmit = handleSubmit(data => {
+        console.log(data)
+    })
+
+    console.log(errors)
+
+    return (
+        <div className="h-[calc(100vh-7rem)] flex justify-center items-center">
+            <form onSubmit={onSubmit} className="w-1/4">
+                <h1 className="text-slate-200 font-bold text-4xl mb-4">
+                    Registrate
+                </h1>
+
+                <label htmlFor="username" className="text-slate-500 mb-2 block text-sm">
+                    Nombre Usuario:
+                </label>
+                <input
+                    type="text"
+                    {...register("username", {
+                        required: {
+                            value: true,
+                            message: 'El usuario es obligatorio'
+                        }
+                    })}
+                    className="p-3 rounded block mb-2 bg-slate-900 text-slate-300 w-full"
+                    placeholder="Usuario..."
+                />
+                {errors.username && (
+                    <span className="text-red-400 text-xs">{errors.username.message}</span>
+                )}
+
+                <label htmlFor="email" className="text-slate-500 mb-2 block text-sm">
+                    Correo Electrónico:
+                </label>
+                <input type="email"
+                    {...register("email", {
+                        required: {
+                            value: true,
+                            message: 'El correo es obligatorio'
+                        }
+                    })}
+                    className="p-3 rounded block mb-2 bg-slate-900 text-slate-300 w-full"
+                    placeholder="usuario@correo.com"
+                />
+                {errors.email && (
+                    <span className="text-red-400 text-xs">{errors.email.message}</span>
+                )}
+
+                <label htmlFor="password" className="text-slate-500 mb-2 block text-sm">
+                    Contraseña:
+                </label>
+                <input type="password"
+                    {...register("password", {
+                        required: {
+                            value: true,
+                            message: 'La contraseña es obligatoria'
+                        }
+                    })}
+                    className="p-3 rounded block mb-2 bg-slate-900 text-slate-300 w-full"
+                    placeholder="*********"
+                />
+                {errors.password && (
+                    <span className="text-red-400 text-xs">{errors.password.message}</span>
+                )}
+
+                <label htmlFor="confirmPassword" className="text-slate-500 mb-2 block text-sm">
+                    Confirmar Contraseña:
+                </label>
+                <input type="password"
+                    {...register("confirmPassword", {
+                        required: {
+                            value: true,
+                            message: 'La confirmación es obligatoria'
+                        }
+                    })}
+                    className="p-3 rounded block mb-2 bg-slate-900 text-slate-300 w-full"
+                    placeholder="Contraseña..."
+                />
+                {errors.confirmPassword && (
+                    <span className="text-red-400 text-xs">{errors.confirmPassword.message}</span>
+                )}
+
+                <button
+                    className="w-full bg-blue-500 text-white p-3 rounded-lg mt-2"
+                >Registrarse
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default RegisterPage
